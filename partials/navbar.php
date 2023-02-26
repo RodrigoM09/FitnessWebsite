cf5cc005ae883fd043a1747aec0109708b3453ba
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-02-26 14:20:29 -0800

    autosave script

---
+++ partials/navbar.php
@@ -0,0 +1,14 @@
+<nav>
+        <div class="container nav_container">
+            <a href="index.php" class="nav_logo">The Temple</a>
+            <ul class="navItems">
+                <li><a href="/views/index.php">Home</a></li>
+                <li><a href="/views/clients.php">Clients</a></li>
+                <li><a href="/views/trainers.php">Trainers</a></li>
+                <li><a href="/views/nutrition.php">Nutrition</a></li>
+                <li><a href="/views/fitness.php">Fitness</a></li>
+                <li><a href="/views/login.php">Login</a></li>
+                <li><a href="/views/Logout.php">Logout</a></li>
+            </ul>
+        </div>
+</nav>
