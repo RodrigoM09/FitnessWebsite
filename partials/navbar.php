cd012070022fc7101e46ba36d15d477229090335
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-02-26 16:03:39 -0800

    autosave script

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar">
         <div class="container nav_container">
             <a href="index.php" class="nav_logo">The Temple</a>
-            <ul class="navItems">
+            <ul class="nav_items">
                 <li><a href="/views/index.php">Home</a></li>
                 <li><a href="/views/clients.php">Clients</a></li>
                 <li><a href="/views/trainers.php">Trainers</a></li>
@@ -13,13 +13,12 @@
                         <img src="/images/avatar1.png" alt="">
                     </div>
                     <ul>
-                        <li><a href="/views/dashboard.php">Dashoboard</a></li>
+                        <li><a href="/views/dashboard.php">Dashboard</a></li>
                         <li><a href="/views/logout.php">logout</a></li>
                     </ul>
                 </li>
             </ul>
             <button id="open_nav_btn"><i class='bx bx-menu'></i></button>
             <button id="close_nav_btn"><i class='bx bx-x' ></i></button>
-
         </div>
 </nav>
