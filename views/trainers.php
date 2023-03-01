8df692253a5e4ab51534f7a3dafa7ff8f4d84f4a
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-03-01 12:02:07 -0800

    autosave script

--- views/trainers.php
+++ views/trainers.php
@@ -5,8 +5,76 @@
 <!------------------------------------NAVBAR PARTIAL----------------------------->
  <?php require('../partials/navbar.php') ?>
 <!------------------------------------NAVBAR PARTIAL----------------------------->
-<section class="empty_page">
-    <h1>Trainers Page</h1>
+<section class="dashboard">
+    <div class="container dashboard_container">
+        <button id="show_sidebar_btn" class="sidebar_toggle"><i class='bx bxs-chevron-right'></i></button>
+        <button id="hide_sidebar_btn" class="sidebar_toggle"><i class='bx bxs-chevron-left'></i></button>
+        <aside>
+            <ul>
+                <li>
+                    <a href="add-post.php"><i class='bx bx-pencil'></i>
+                        <h5>Add Post</h5>
+                </a></li>
+                <li>
+                    <a href="dashboard.php"><i class='bx bxs-dashboard'></i>
+                        <h5>Manage Posts</h5>
+                </a></li>
+                <li>
+                    <a href="add-user.php"><i class='bx bx-user-circle'></i>
+                        <h5>Add User</h5>
+                </a></li>
+                <li>
+                    <a href="manage-users.php"><i class='bx bxs-user-account'></i>
+                        <h5>Manage User</h5>
+                </a></li>
+                <li>
+                    <a href="add-category.php"><i class='bx bx-category-alt'></i>
+                        <h5>Add Category</h5>
+                </a></li>
+                <li>
+                    <a href="manage-categories.php" class="active"><i class='bx bx-category'></i>
+                        <h5>Manage Categories</h5>
+                </a></li>
+            </ul>
+        </aside>
+        <main>
+            <h2>Manage Clients</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Username</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Armando Cruz</td>
+                        <td>Armandy</td>
+                        <td><a href="edit-category.php" class="btn sm">Edit</a></td>
+                        <td><a href="delete-category.php" class="btn sm danger">Delete</a></td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>Rodrigo Marquez</td>
+                        <td>Rigo</td>
+                        <td><a href="edit-category.php" class="btn sm">Edit</a></td>
+                        <td><a href="delete-category.php" class="btn sm danger">Delete</a></td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>David Cruz</td>
+                        <td>Davey</td>
+                        <td><a href="edit-category.php" class="btn sm">Edit</a></td>
+                        <td><a href="delete-category.php" class="btn sm danger">Delete</a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </main>
+    </div>
 </section>
 <!------------------------------------FOOTER PARTIAL----------------------------->
     <?php require('../partials/footer.php') ?>
